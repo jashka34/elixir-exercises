ec36e7cd9fb93b88bf54bad25f4cc45669439902
jashka34 <a_cherniadiev@mail.ru>
2025-03-16 08:32:48 +0300

    35/50

--- code_basics/lib/solution.ex
+++ code_basics/lib/solution.ex
@@ -1,7 +1,27 @@
 defmodule Solution do
   # 37/50
-  def calculate(f) do
-    0
+  def calculate(parent) do
+    receive do
+      {:+, [x, y]} ->
+        send(parent, {:ok, x + y})
+        calculate(parent)
+
+      {:-, [x, y]} ->
+        send(parent, {:ok, x - y})
+        calculate(parent)
+
+      {:/, [x, y]} ->
+        send(parent, {:ok, x / y})
+        calculate(parent)
+
+      {:*, [x, y]} ->
+        send(parent, {:ok, x * y})
+        calculate(parent)
+
+      {:exit} ->
+        send(parent, {:ok, :exited})
+        # IO.puts("exit...")
+    end
   end
 
   # 36/50
