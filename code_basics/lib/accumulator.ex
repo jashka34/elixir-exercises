ddcde643030d851e7b23ecd6cf61a59df784352b
jashka34 <a_cherniadiev@mail.ru>
2025-03-20 02:35:58 +0300

    40/50

--- code_basics/lib/accumulator.ex
+++ code_basics/lib/accumulator.ex
@@ -1,2 +1,37 @@
 defmodule Accumulator do
+  def start_link(initial_state) do
+    Agent.start_link(fn -> initial_state end, name: __MODULE__)
+  end
+
+  def current do
+    Agent.get(__MODULE__, fn state -> state end)
+  end
+
+  def add(n) do
+    Agent.update(__MODULE__, fn state ->
+      Calculator.exec(:+, state, n)
+    end)
+  end
+
+  def mul(n) do
+    Agent.update(__MODULE__, fn state ->
+      Calculator.exec(:*, state, n)
+    end)
+  end
+
+  def sub(n) do
+    Agent.update(__MODULE__, fn state ->
+      Calculator.exec(:-, state, n)
+    end)
+  end
+
+  def div(n) do
+    Agent.update(__MODULE__, fn state ->
+      Calculator.exec(:/, state, n)
+    end)
+  end
+
+  def reset do
+    Agent.update(__MODULE__, fn _ -> 0 end)
+  end
 end
--- code_basics/test/code_basics_test.exs
+++ code_basics/test/code_basics_test.exs
@@ -24,33 +24,33 @@ defmodule CodeBasicsTest do
       assert Calculator.exec(:/, 20, 10) == 2
     end
 
-    # test "accumulator agent" do
-    #   Accumulator.start_link(0)
-    #
-    #   Accumulator.add(10)
-    #   assert Accumulator.current() == 10
-    #   Accumulator.add(3)
-    #   assert Accumulator.current() == 13
-    #
-    #   Accumulator.mul(10)
-    #   assert Accumulator.current() == 130
-    #   Accumulator.mul(3)
-    #   assert Accumulator.current() == 390
-    #
-    #   Accumulator.sub(10)
-    #   assert Accumulator.current() == 380
-    #   Accumulator.sub(3)
-    #   assert Accumulator.current() == 377
-    #
-    #   Accumulator.div(10)
-    #   assert Accumulator.current() == 37
-    #   Accumulator.div(3)
-    #   assert Accumulator.current() == 12
-    #
-    #   Accumulator.reset()
-    #   assert Accumulator.current() == 0
-    #   assert Agent.stop(Accumulator) == :ok
-    # end
+    test "accumulator agent" do
+      Accumulator.start_link(0)
+
+      Accumulator.add(10)
+      assert Accumulator.current() == 10
+      Accumulator.add(3)
+      assert Accumulator.current() == 13
+
+      Accumulator.mul(10)
+      assert Accumulator.current() == 130
+      Accumulator.mul(3)
+      assert Accumulator.current() == 390
+
+      Accumulator.sub(10)
+      assert Accumulator.current() == 380
+      Accumulator.sub(3)
+      assert Accumulator.current() == 377
+
+      Accumulator.div(10)
+      assert Accumulator.current() == 37
+      Accumulator.div(3)
+      assert Accumulator.current() == 12
+
+      Accumulator.reset()
+      assert Accumulator.current() == 0
+      assert Agent.stop(Accumulator) == :ok
+    end
   end
 
   test "39/50 foobar supervisor work" do
